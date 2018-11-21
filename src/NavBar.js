6f3fd3e62f5a0e6191fd044948359ab1bac084a9
crocdialer <crocdialer@gmail.com>
2018-11-21 23:13:15 +0100

    command interface via dropdown menu

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,28 @@
+import $ from 'jquery';
 import React, { Component } from 'react';
 import './NavBar.css';
 
+function NodeMenuItem(props){
+    return(
+      <li className="dropdown-item" onClick={props.onClick}>
+        <a href="#">{props.title}</a>
+      </li>
+    );
+}
+
 class NavBar extends Component {
   render() {
+    let apiHost = "http://" + window.location.hostname + ":8080"
+    let recordMenuItems = this.props.nodeList.map((n) =>{
+      let clickHandler = () => fetch(apiHost + "/nodes/cmd?address=" + n.address +"&action=record&duration=10s")
+      return(
+        <NodeMenuItem
+          title={"Node " + n.address}
+          onClick={clickHandler}
+        />
+      );
+    });
+
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/">LoRa Gateway</a>
@@ -12,31 +32,57 @@ class NavBar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <a className="nav-link" href="/nodes">Nodes</a>
-            </li>
+            </li> */}
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Command
               </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/nodes/cmd">Record</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                <li className="dropdown-item dropdown-submenu">
+                  <a href="#" data-toggle="dropdown" class="dropdown-toggle">Record</a>
+                  <ul className="dropdown-menu">
+                    {recordMenuItems}
+                  </ul>
+                </li>
+
+                {/* <li>
+                  <a className="dropdown-item" href="#" onClick={() => fetch(apiHost + "/nodes/cmd")}>Record</a>
+                </li> */}
+                <li>
+                  <a className="dropdown-item" href="#">Another action</a>
+                </li>
+                {/* <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Something else here</a> */}
+              </ul>
             </li>
             <li className="nav-item">
               <a className="nav-link disabled" href="#">Disabled</a>
             </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
+          {/* <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          </form> */}
         </div>
       </nav>
     );
+  }
+
+  componentDidMount(){
+    $('.dropdown-submenu > a').on("click", function(e) {
+      var submenu = $(this);
+      $('.dropdown-submenu .dropdown-menu').removeClass('show');
+      submenu.next('.dropdown-menu').addClass('show');
+      e.stopPropagation();
+    });
+
+    $('.dropdown').on("hidden.bs.dropdown", function() {
+      // hide any open menus when parent closes
+      $('.dropdown-menu.show').removeClass('show');
+    });
   }
 }
 
--- src/NodeComponent.js
+++ src/NodeComponent.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 import './NodeComponent.css';
 
-class NodeComponent extends Component {
+function Node(props){
+    return(
+      <div className="row">
+        <div className="col-md-12 node">
+          <p>Node {props.value.address}</p>
+          <ul>
+            <li>address: {props.value.address}</li>
+            <li>rssi: {props.value.rssi}</li>
+            <li>battery: {props.value.bat}</li>
+            <li>freq: 434 MHz</li>
+            <li>mode: {props.value.mode}</li>
+            <li>last timestamp: {props.value.stamp}</li>
+          </ul>
+        </div>
+      </div>
+    );
+}
+
+export default class NodeComponent extends Component {
+
+  renderNode(nodeData){
+    return(
+      <Node
+         key={nodeData.address}
+         value={nodeData}
+      />
+    );
+  }
+
   render() {
-    return <div>NodeComponent</div>
+    let nodes = this.props.nodeList.map((n) => {
+      return this.renderNode(n);
+    });
+
+    return(
+      <div className="container">
+        <h1>
+          Nodes
+        </h1>
+        {nodes}
+      </div>
+    );
   }
 };
 
-export default NodeComponent;
+// export default NodeComponent;
