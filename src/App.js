d6881a48b59915571b900ff55f59ee381f92b9a3
crocdialer <crocdialer@gmail.com>
2018-11-21 16:53:29 +0100

    initial commit for loRanger webapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
+import NodeComponent from './NodeComponent';
 
 class App extends Component {
+  state = {
+    data:[{"address":1,"rssi":-30,"mode":0,"temp":0,"bat":0.290323,"stamp":"2018-11-21T16:43:39.058182298+01:00","gps":[0,0]},
+          {"address":23,"rssi":-54,"mode":0,"temp":0,"bat":1,"stamp":"2018-11-21T16:43:39.706666293+01:00","gps":[0,0]}]
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <NavBar />
+        <NodeComponent />
       </div>
     );
   }
---
+++ src/NodeComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './NodeComponent.css';
+
+class NodeComponent extends Component {
+  render() {
+    return <div>NodeComponent</div>
+  }
+};
+
+export default NodeComponent;
