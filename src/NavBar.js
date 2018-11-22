5b1d0cb4a85c713905c24f3758e2590d5f16e7e3
crocdialer <crocdialer@gmail.com>
2018-11-22 15:41:15 +0100

    overall progress with ui, juggling with React component creation

--- src/NavBar.js
+++ src/NavBar.js
@@ -13,16 +13,35 @@ function NodeMenuItem(props){
 class NavBar extends Component {
   render() {
     let apiHost = "http://" + window.location.hostname + ":8080"
+
+    // items for the record dropdown menu
     let recordMenuItems = this.props.nodeList.map((n) =>{
-      let clickHandler = () => fetch(apiHost + "/nodes/cmd?address=" + n.address +"&action=record&duration=10s")
+      if(!n.active) {
+        return;
+      }
+      let clickHandler = () => fetch(apiHost + "/nodes/cmd?address=" + n.address +"&action=record&value=1&duration=20s")
       return(
         <NodeMenuItem
+          key={n.address}
           title={"Node " + n.address}
           onClick={clickHandler}
         />
       );
     });
-
+    // items for the flashlight dropdown menu
+    let flashMenuItems = this.props.nodeList.map((n) =>{
+      if(!n.active) {
+        return;
+      }
+      let clickHandler = () => fetch(apiHost + "/nodes/cmd?address=" + n.address +"&action=flashlight&value=1")
+      return(
+        <NodeMenuItem
+          key={n.address}
+          title={"Node " + n.address}
+          onClick={clickHandler}
+        />
+      );
+    });
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/">LoRa Gateway</a>
@@ -32,57 +51,29 @@ class NavBar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            {/* <li className="nav-item">
-              <a className="nav-link" href="/nodes">Nodes</a>
-            </li> */}
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Command
+                Record
               </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-
-                <li className="dropdown-item dropdown-submenu">
-                  <a href="#" data-toggle="dropdown" class="dropdown-toggle">Record</a>
-                  <ul className="dropdown-menu">
-                    {recordMenuItems}
-                  </ul>
-                </li>
-
-                {/* <li>
-                  <a className="dropdown-item" href="#" onClick={() => fetch(apiHost + "/nodes/cmd")}>Record</a>
-                </li> */}
-                <li>
-                  <a className="dropdown-item" href="#">Another action</a>
-                </li>
-                {/* <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a> */}
+                {recordMenuItems}
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Flashlight
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                {flashMenuItems}
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
+              <a className="nav-link disabled" href="/">Disabled</a>
             </li>
           </ul>
-          {/* <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form> */}
         </div>
       </nav>
     );
-  }
-
-  componentDidMount(){
-    $('.dropdown-submenu > a').on("click", function(e) {
-      var submenu = $(this);
-      $('.dropdown-submenu .dropdown-menu').removeClass('show');
-      submenu.next('.dropdown-menu').addClass('show');
-      e.stopPropagation();
-    });
-
-    $('.dropdown').on("hidden.bs.dropdown", function() {
-      // hide any open menus when parent closes
-      $('.dropdown-menu.show').removeClass('show');
-    });
   }
 }
 
--- src/NodeComponent.js
+++ src/NodeComponent.js
@@ -1,25 +1,45 @@
+import $ from 'jquery';
 import React, { Component } from 'react';
 import './NodeComponent.css';
 
 function Node(props){
+  let batteryStr = Math.round(props.value.bat * 100) + " %";
+  let className = "col-md-6 node" + (props.value.active ? " active" : "") + (props.value.mode ? " record" : "");
     return(
-      <div className="row">
-        <div className="col-md-12 node">
+        <div className={className}>
           <p>Node {props.value.address}</p>
           <ul>
+            <li>id: {props.value.id}</li>
             <li>address: {props.value.address}</li>
             <li>rssi: {props.value.rssi}</li>
-            <li>battery: {props.value.bat}</li>
+            <li>battery: {batteryStr}</li>
             <li>freq: 434 MHz</li>
             <li>mode: {props.value.mode}</li>
             <li>last timestamp: {props.value.stamp}</li>
           </ul>
         </div>
-      </div>
     );
 }
 
 export default class NodeComponent extends Component {
+
+  renderNodeGroup(){
+    let nodes = this.props.nodeList.map((n) => {
+      return this.renderNode(n);
+    });
+
+    let outList = []
+    for (var i = 0; i < nodes.length; i+=2) {
+      let children = [nodes[i]]
+      
+      if(i + 1 < nodes.length){
+        children.push(nodes[i + 1])
+      }
+      let row = React.createElement('div', {className: 'row'}, children)
+      outList.push(row)
+    }
+    return outList;
+  }
 
   renderNode(nodeData){
     return(
@@ -31,16 +51,12 @@ export default class NodeComponent extends Component {
   }
 
   render() {
-    let nodes = this.props.nodeList.map((n) => {
-      return this.renderNode(n);
-    });
-
     return(
       <div className="container">
         <h1>
           Nodes
         </h1>
-        {nodes}
+        {this.renderNodeGroup()}
       </div>
     );
   }
